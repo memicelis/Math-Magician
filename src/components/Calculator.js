3b4134e63f88758433fdcd01403c0952aa3224ba
Elis Memic <elismemic5@gmail.com>
2023-09-15 12:39:13 +0200

    update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,72 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
+import Button from './Button';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="display">
-      <input className="input-display" type="text" />
+const Calculator = () => {
+  const [calcData, setCalcData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const [display, setDisplay] = useState('');
+
+  const handleButtonClick = (value) => {
+    const newCalcData = calculate(calcData, value);
+    setCalcData(newCalcData);
+    setDisplay(newCalcData.next || newCalcData.total || '0');
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input className="input-display" type="text" value={display} />
+      </div>
+      <div className="buttons">
+        <Button value="AC" onClick={handleButtonClick} />
+        <Button value="+/-" onClick={handleButtonClick} />
+        <Button value="%" onClick={handleButtonClick} />
+        <Button value="÷" onClick={handleButtonClick} />
+        <Button value="7" onClick={handleButtonClick} />
+        <Button value="8" onClick={handleButtonClick} />
+        <Button value="9" onClick={handleButtonClick} />
+        <Button value="x" onClick={handleButtonClick} />
+        <Button value="4" onClick={handleButtonClick} />
+        <Button value="5" onClick={handleButtonClick} />
+        <Button value="6" onClick={handleButtonClick} />
+        <Button value="-" onClick={handleButtonClick} />
+        <Button value="1" onClick={handleButtonClick} />
+        <Button value="2" onClick={handleButtonClick} />
+        <Button value="3" onClick={handleButtonClick} />
+        <Button value="+" onClick={handleButtonClick} />
+        <Button value="0" onClick={handleButtonClick} />
+        <Button value="." onClick={handleButtonClick} />
+        <Button value="=" onClick={handleButtonClick} />
+      </div>
     </div>
-    <div className="buttons">
-      <button className="calculator-button" type="button">
-        AC
-      </button>
-      <button className="calculator-button" type="button">
-        +/-
-      </button>
-      <button className="calculator-button" type="button">
-        %
-      </button>
-      <button className="calculator-button" type="button">
-        ÷
-      </button>
-      <button className="calculator-button" type="button">
-        7
-      </button>
-      <button className="calculator-button" type="button">
-        8
-      </button>
-      <button className="calculator-button" type="button">
-        9
-      </button>
-      <button className="calculator-button" type="button">
-        x
-      </button>
-      <button className="calculator-button" type="button">
-        4
-      </button>
-      <button className="calculator-button" type="button">
-        5
-      </button>
-      <button className="calculator-button" type="button">
-        6
-      </button>
-      <button className="calculator-button" type="button">
-        -
-      </button>
-      <button className="calculator-button" type="button">
-        1
-      </button>
-      <button className="calculator-button" type="button">
-        2
-      </button>
-      <button className="calculator-button" type="button">
-        3
-      </button>
-      <button className="calculator-button" type="button">
-        +
-      </button>
-      <button className="calculator-button calculator-button--0" type="button">
-        0
-      </button>
-      <button className="calculator-button" type="button">
-        .
-      </button>
-      <button className="calculator-button" type="button">
-        =
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
